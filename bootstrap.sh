cd51bdc9d40e991b1f1a4743ef28ac9d91f5fa5c
edge2992 <torune.summer@gmail.com>
2023-07-03 21:18:33 +0900

    feat(bootstrap.sh): add bootstrap script to download and install tools for arm64 architecture
    The bootstrap.sh script is added to the project. This script is responsible for downloading and installing tools specifically for the arm64 architecture. The script supports the following commands:
    
    - `download`: Downloads and installs tools using a shell script. Specifically, it installs `alp` tool.
    - `quit`: Exits the script.
    
    The script checks if the architecture is arm64 before proceeding with the installation. If the architecture is not arm64, an error message is displayed and the script exits.
    
    The `alp` tool is downloaded from the specified URL and installed. If the installation is successful, a success message is displayed. If any unexpected error occurs during the installation, an error message is displayed.
    
    The script also provides a usage message and prompts the user for a command to execute. If an invalid command is entered, an error message is displayed and the user is prompted again.
    
    The script

---
+++ bootstrap.sh
@@ -0,0 +1,120 @@
+#!/bin/bash -eu
+# shellcheck disable=SC2317
+
+
+# tool_download_path
+ALP_SOURCE_URL="https://github.com/tkuchiki/alp/releases/download/v1.0.12/alp_linux_arm64.zip"
+
+# tool install path
+
+if which tput >/dev/null 2>&1; then
+  ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+  RED="$(tput setaf 1)"
+  GREEN="$(tput setaf 2)"
+  YELLOW="$(tput setaf 3)"
+  BLUE="$(tput setaf 4)"
+  BOLD="$(tput bold)"
+  NORMAL="$(tput sgr0)"
+else
+  RED=""
+  GREEN=""
+  YELLOW=""
+  BLUE=""
+  BOLD=""
+  NORMAL=""
+fi
+
+# funciton
+
+has() {
+  type "$1" > /dev/null 2>&1
+}
+
+dotfiles() {
+  echo $BLUE
+  cat <<\EOF
+ _                                        _
+(_)___ _   _  ___ ___  _ __      ___  ___| |_ _   _ _ __
+| / __| | | |/ __/ _ \| '_ \    / __|/ _ \ __| | | | '_ \
+| \__ \ |_| | (_| (_) | | | |   \__ \  __/ |_| |_| | |_) |
+|_|___/\__,_|\___\___/|_| |_|___|___/\___|\__|\__,_| .__/
+                           |_____|                 |_|
+EOF
+  echo $NORMAL
+}
+
+usage() {
+  echo $YELLOW
+  cat <<\EOF
+Commands:
+  download (download tools by shellscript. alp would be installed.)
+  quit
+EOF
+  echo $NORMAL
+}
+
+check_arch() {
+  if [ "$(uname -m)" = "arm64" ]; then
+    echo "${GREEN}arm64 detected.$NORMAL"
+  else
+    echo "${RED}arm64 not detected. This script is only runnable in arm64.$NORMAL"
+    exit 1
+  fi
+}
+
+alp_download() {
+  pushd tmp
+  wget -nv "$ALP_SOURCE_URL"
+  unzip "$(basename "$ALP_SOURCE_URL")"
+  sudo install ./alp /usr/local/bin/alp
+  rm "$(basename "$ALP_SOURCE_URL")"
+  rm alp
+  popd
+
+  if [ $? = 0 ]; then
+    echo "${GREEN}Successfully installed alp. ✔︎ $NORMAL"
+  else
+    echo "${RED}An unexpected error occurred when trying to install alp.$NORMAL"
+  fi
+}
+
+download() {
+  # check if arch is arm64
+  check_arch
+  
+  cd "$HOME"
+  mkdir -p tmp
+
+  # alp
+  if ! has "alp"; then
+    alp_download
+  else
+    echo "${BOLD}alp already exists.$NORMAL"
+  fi
+}
+
+
+# main
+main() {
+  usage
+  echo -n "${BOLD}command: $NORMAL"
+  read command
+  case $command in
+    quit)
+      echo "bye!"
+      exit 0
+      ;;
+    download)
+      download
+      ;;
+    *)
+      echo "${RED}bootstrap: command not found.$NORMAL"
+      main
+      ;;
+  esac
+}
+dotfiles
+main
+exit 0
